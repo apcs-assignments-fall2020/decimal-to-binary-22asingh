fd624cbc5a2f230a47e12fe5dc5c70b88aabf05e
22asingh <22asingh@athenian.org>
2020-09-15 21:01:11 +0000

    defined binary to decimal method and tested it in main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int len = binary.length();
+        int bi = Integer.parseInt(binary);
+        int dec = 0;
+        for (int i = 0; i < len; i++){
+            dec += ((bi/(int)Math.pow(10,i))%10) * Math.pow(2,i);
+        }
+        return dec;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a binary number: ");
+        String binary = scan.next();
+        int decimal = binaryToDecimal(binary);
+        System.out.println("That is equal to the decimal value: " + decimal);
+        scan.close();
+
     }
 }
